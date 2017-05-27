09f054127e7dbda461375a57b9d6aa86a1a1177c
ht-zou <sireniazoe@zouhaitians-MacBook-Air.local>
2017-05-27 15:48:30 -0700

    Added some model class templates

--- src/model/AbstractBoard.java
+++ src/model/AbstractBoard.java
@@ -7,6 +7,8 @@ public abstract class AbstractBoard {
 	protected static final int width = 15;
 	protected static final int height = 15;
 	protected static final int invalid_location = 225;
+	protected static final int sente_stone = 3;
+	protected static final int gote_stone = 2;
 	protected int[] rowBased;
 	protected int[] colBased;
 	protected int[] ltorDiag;
@@ -34,17 +36,37 @@ public abstract class AbstractBoard {
 		return true;
 	}
 	
-	public boolean updateBoard(int location) {
+	public boolean updateBoard(int location, boolean first) {
 		if (location < 0 || location >= invalid_location)
 			return false;
 		int rowIndex = location / width;
 		int colIndex = location % width;
+		int ltorIndex = rowIndex - colIndex + width - 1;
+		int rtolIndex = rowIndex + colIndex;
 		int origRow = rowBased[rowIndex];
 		int origCol = colBased[colIndex];
-		int origLtoRDiag = ltorDiag[rowIndex - colIndex + width - 1];
-		int origRtoLDiag = rtolDiag[rowIndex + colIndex];
+		int origLtoRDiag = ltorDiag[ltorIndex];
+		int origRtoLDiag = rtolDiag[rtolIndex];
+
+		int indexOnLtoRDiag = ltorIndex < width ? rowIndex : colIndex;
+		int indexOnRtoLDiag = rtolIndex < width ? rowIndex : width - 1 - colIndex;
+
+		if ((rowIndex & (3 << (colIndex * 2))) != 0)
+			return false;
+		int stone = first ? sente_stone : gote_stone;
+
+		origRow = origRow ^ (stone << colIndex * 2);
+		rowBased[rowIndex] = origRow;
+
+		origCol = origCol ^ (stone << rowIndex * 2);
+		colBased[colIndex] = origCol;
+
+		origLtoRDiag = origLtoRDiag ^ (stone << rtolIndex * 2);
+		ltorDiag[ltorIndex] = origLtoRDiag;
+
+		origRtoLDiag = origRtoLDiag ^ (stone << ltorIndex * 2);
+		rtolDiag[rtolIndex] = origRtoLDiag;
 		
-		
-		return false;
+		return true;
 	}
 }
